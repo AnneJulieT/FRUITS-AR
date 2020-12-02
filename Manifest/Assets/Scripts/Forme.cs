c8a6354815168bb444ed498937792a200c69e489
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-12-02 14:26:24 -0500

    Added Text info

--- Manifest/Assets/Scripts/Forme.cs
+++ Manifest/Assets/Scripts/Forme.cs
@@ -5,16 +5,5 @@ using UnityEngine;
 public class Forme : MonoBehaviour
 {
     public GameObject _origin;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] GameObject _description;
 }
