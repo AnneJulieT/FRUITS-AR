25ccaab8625dc7bb7ac1891a53d4c24f1616d335
Anne-Julie <anne-julie.tremblay@pixsenses.com>
2021-04-07 19:39:16 -0400

    scene anneju cocktail pas mal fini

---
+++ fruit_standard/Assets/pourritureController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pourritureController : MonoBehaviour
+{
+    [SerializeField] private GameObject smoke;
+    [SerializeField] private BottleSmash bottleSmash;
+    [SerializeField] private LiquidVolumeAnimator lva;
+    [SerializeField] private GameObject cocktailWrapper;
+
+    private void Start()
+    {
+        smoke.SetActive(false);
+        cocktailWrapper.SetActive(false);
+        bottleSmash.color = new Color(Random.Range(0,0.9f), Random.Range(0,0.5f), Random.Range(0.05f,0.9f)); 
+        lva.level = 0.68f;
+    }
+    public void Appear() {
+        bottleSmash.color = new Color(Random.Range(0, 0.9f), Random.Range(0, 0.5f), Random.Range(0.05f, 0.9f));
+        lva.level = 0.68f;
+        smoke.SetActive(true);
+        cocktailWrapper.SetActive(true);
+    }
+
+    public void Disapear() {
+        smoke.SetActive(false);
+        cocktailWrapper.SetActive(false);
+    }
+
+}
