a230683bbe2ee807ae690392e2f7ea05ed4733eb
Anne-Julie <anne-julie.tremblay@pixsenses.com>
2021-04-10 16:15:09 -0400

    instantiate cherry + parapluie collider

--- fruit_standard/Assets/pourritureController.cs
+++ fruit_standard/Assets/pourritureController.cs
@@ -8,6 +8,10 @@ public class pourritureController : MonoBehaviour
     [SerializeField] private BottleSmash bottleSmash;
     [SerializeField] private LiquidVolumeAnimator lva;
     [SerializeField] private GameObject cocktailWrapper;
+    [SerializeField] private GameObject cherryPrefab;
+    [SerializeField] private GameObject parapluie;
+    [SerializeField] GameObject instRef;
+    private ArrayList cherryList = new ArrayList();
 
     private void Start()
     {
@@ -16,6 +20,19 @@ public class pourritureController : MonoBehaviour
         bottleSmash.color = new Color(Random.Range(0,0.9f), Random.Range(0,0.5f), Random.Range(0.05f,0.9f)); 
         lva.level = 0.68f;
     }
+
+    private void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                GameObject go = Instantiate(cherryPrefab, instRef.transform.position, Quaternion.identity);
+                go.transform.localScale = new Vector3(9, 9, 9);
+                cherryList.Add(go);
+            }
+        }
+        }
     public void Appear() {
         bottleSmash.color = new Color(Random.Range(0, 0.9f), Random.Range(0, 0.5f), Random.Range(0.05f, 0.9f));
         lva.level = 0.68f;
@@ -26,6 +43,18 @@ public class pourritureController : MonoBehaviour
     public void Disapear() {
         smoke.SetActive(false);
         cocktailWrapper.SetActive(false);
+        parapluie.transform.localPosition = new Vector3(-0.00306017604f, 0.00423249509f, 0.0045483131f);
+        parapluie.transform.localRotation = new Quaternion(-0.527507663f, 0.0597384758f, -0.800903678f, 0.276984423f);
+        for (int i = 0; i < cherryList.Count; i++) {
+            Destroy((GameObject)cherryList[i]);
+        }
+        cherryList.Clear();
     }
 
+    public void InstanciateCherry() {
+        
+        GameObject go = Instantiate(cherryPrefab, instRef.transform.position, Quaternion.identity);
+        go.transform.localScale = new Vector3(9, 9, 9);
+        cherryList.Add(go);
+    }
 }
