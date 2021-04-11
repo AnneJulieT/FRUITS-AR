81daa86934a41035d66c4548a0bf97d3479ca054
Catherine Begin <catherinebegin18@gmail.com>
2021-04-11 11:18:54 -0700

    Scene Cat - paroxysme

---
+++ fruit_standard/Assets/flot_v2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flot_v2 : MonoBehaviour
+{
+
+    public Transform prefabBulle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startCoroutine("popBubble");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            InvokeRepeating("popBubble", 0f, 0.2f);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            CancelInvoke("popBubble");
+        }
+
+    }
+
+
+    IEnumerator popBubble()
+    {
+        while (true) 
+        {
+            Transform orangeBub = Instantiate(prefabBulle);
+            orangeBub.position = new Vector3(0f, 6f, 0f);
+            orangeBub.rotation = Quaternion.Euler(new Vector3(0f, 0f, 35f));
+
+            Destroy(orangeBub.gameObject, 5f);
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    
+    }
+
+}
